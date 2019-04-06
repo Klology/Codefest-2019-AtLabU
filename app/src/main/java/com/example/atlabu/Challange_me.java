b5a9fcb8d4b0ada9f794aef78b64df06ccfb1535
extrafot <extrafot@gmail.com>
2019-04-05 22:26:28 -0500

    Added Chalenge me area

--- app/src/main/java/com/example/atlabu/Challange_me.java
+++ app/src/main/java/com/example/atlabu/Challange_me.java
@@ -1,6 +1,5 @@
 package com.example.atlabu;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Challange_me extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -19,19 +18,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText("More!");
-
-
-
-
+                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText("Challenge Me!");
-                    //startActivity(new Intent(MainActivity.this, My_Courses.class));
-                    startActivity(new Intent(MainActivity.this, Challange_me.class));
+                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText("Profile");
+                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -41,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_challange_me);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
