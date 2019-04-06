bb5513cdd9aa38b3d287af5f03c2c66cbca7d91d
Sarah <SarahBui477@gmail.com>
2019-04-06 14:42:31 -0500

    Shop Display created

--- app/src/main/java/com/example/atlabu/Shop.java
+++ app/src/main/java/com/example/atlabu/Shop.java
@@ -1,10 +1,12 @@
 package com.example.atlabu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 
@@ -37,6 +39,14 @@ public class Shop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*
+        coupon_button= findViewById(R.id.coupon_button);
+        coupon_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(Shop.this, shop_code.class));
+            }
+        }); */
 
         setContentView(R.layout.activity_shop);
 
